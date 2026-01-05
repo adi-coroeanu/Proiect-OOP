6d5e3c12507fe6f55cf4e2a4591317dd20a2e538
Vramita Darius <darius_adrian10@yahoo.com>
2026-01-05 13:07:02 +0200

    Indicatii

--- Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Entities/Rezervare.cs
+++ Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Entities/Rezervare.cs
@@ -2,9 +2,9 @@ namespace SistemRezervari.CORE.Entities;
 
 public record Rezervare(
     Guid Id,
-    Guid TerenId,        // Legătura cu terenul
-    Guid UtilizatorId,   // Cine a făcut rezervarea
-    DateTime DataInceput,// ex: 2023-10-10 14:00
-    DateTime DataSfarsit,// ex: 2023-10-10 16:00
-    bool EsteAnulata     // Dacă clientul a renunțat la ea
+    Guid TerenId,        
+    Guid UtilizatorId,   
+    DateTime DataInceput,
+    DateTime DataSfarsit,
+    bool EsteAnulata     
 );
