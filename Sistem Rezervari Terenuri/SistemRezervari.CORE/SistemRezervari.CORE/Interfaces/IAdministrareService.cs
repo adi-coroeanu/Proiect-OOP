5aa4270a97fc280cf47161cd70684028f86afeb3
Filca Alexandru <alexandru.filca@gmail.com>
2026-01-05 14:13:54 +0200

    Update interfata

--- Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Interfaces/IAdministrareService.cs
+++ Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Interfaces/IAdministrareService.cs
@@ -3,4 +3,8 @@ namespace SistemRezervari.CORE.Interfaces;
 public interface IAdministrareService
 {
     //metode precum void Adaugare(), Stegere(),Vizualizare() etc etc
+	public void AddField();
+	public void RemoveField();
+	public void ModifyField();
+
 }
