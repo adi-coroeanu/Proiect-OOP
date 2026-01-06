bf8e0b1ad97394dc2217969f9e9706d590b4a11f
Filca Alexandru <alexandru.filca@gmail.com>
2026-01-06 18:39:02 +0200

    Update ReservationAdministration

--- Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/AdministrationLogic/ReservationAdministration.cs
+++ Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/AdministrationLogic/ReservationAdministration.cs
@@ -8,16 +8,37 @@ public class ReservationAdministration : IReservationAdministration
 {
     private ReservationRepository _reservationRepository;
     private List<Rezervare> _reservations;
+    private List<Rezervare> _reservations_with_specific_fieldID = new List<Rezervare>();
     public ReservationAdministration(ReservationRepository  reservationRepository)
     {
        _reservationRepository = reservationRepository; 
        _reservations = _reservationRepository.GetCopyAll();
     }
 
+    #region Private Methods
+    private void _RemoveReservation(Guid reservationId)
+    {
+        for (int i = 0; i < _reservations.Count; i++)
+        {
+            if (_reservations[i].Id == reservationId)
+                _reservations.RemoveAt(i);
+        }
+    }
+
+    private void _GetAllReservations(Guid terenId)
+    {
+        foreach (var reservation in _reservations)
+            if(reservation.TerenId == terenId)
+                _reservations_with_specific_fieldID.Add(reservation);
+    }
+    #endregion
+    
+    
     #region Public Methods
     public void RemoveReservation(Guid reservationId)
     {
-        throw new NotImplementedException();
+        _RemoveReservation(reservationId);
+        _reservationRepository.ModifyList(_reservations);
     }
 
     public void ModifyReservation(Guid reservationId, DateTime from, DateTime to)
@@ -28,7 +49,8 @@ public class ReservationAdministration : IReservationAdministration
     
     public List<Rezervare> GetAllReservations(Guid terenId)
     {
-        return _reservations;
+        _GetAllReservations(terenId);
+        return _reservations_with_specific_fieldID;
     }
    #endregion
 }
--- Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Interfaces/IAdministrareService.cs
+++ Sistem Rezervari Terenuri/SistemRezervari.CORE/SistemRezervari.CORE/Interfaces/IAdministrareService.cs
@@ -11,7 +11,7 @@ public interface IAdministrareService
 	public Teren GetFieldById(Guid terenId);
 	public void ModifyField(Guid terenId,string newFieldName, string newFieldType, int newFieldCapacity, string newFieldProgram,string newFieldRestrictions);
 	//metode la Rezervari
-	public void RemoveReservation(Guid reservationId);
+	public void RemoveReservation(Guid reservationId);	
 	public void ModifyReservation(Guid reservationId, DateTime from, DateTime to);//parametrii vor contine noile date ale unei rezervari
 	public List<Rezervare> GetAllReservations(Guid terenId);
 }
